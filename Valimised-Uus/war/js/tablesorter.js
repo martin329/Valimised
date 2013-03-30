e08ab19062868c2f313e9ba843ef2c3bbb0f9308
asus <asus@192.168.1.70>
2013-03-30 22:21:45 +0200

    Revert "."
    This reverts commit c1c383acd6ee6f5ba6eca24182cf85adcb269ef6.

--- Valimised-Uus/war/js/tablesorter.js
+++
@@ -1,7 +0,0 @@
-<!--Sorteerib tabelit id="kandidaatide-tabel" esimese veeru järgi-->
-$( document ).ready(function() {
-$(function() {
-		$("#kandidaatide-tabel").tablesorter({sortList:[[0,0]],widthFixed:true});
-		console.log("tabel");
-	});
-});
