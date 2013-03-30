99cb9e6f3c32c21f88a76c39fc1515d397e47fec
asus <asus@192.168.1.70>
2013-03-30 23:47:19 +0200

    failid

---
+++ Valimised-Uus/war/js/tablesorter.js
@@ -0,0 +1,7 @@
+<!--Sorteerib tabelit id="kandidaatide-tabel" esimese veeru järgi-->
+$( document ).ready(function() {
+$(function() {
+		$("#kandidaatide-tabel").tablesorter({sortList:[[0,0]],widthFixed:true});
+		console.log("tabel");
+	});
+});
